492e735a21f4d36543cf8b3d2123e023e8be0dde
jawel22 <46859689+jawel22@users.noreply.github.com>
2023-07-24 12:20:02 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xba84e3fe480c49aadaa7b0cb26093cbd90d23e67";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
